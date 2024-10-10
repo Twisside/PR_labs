5314f557d145406e2fed844d868edd0cc1a25c40
Twisside <ana.maria.timciuc.01@gmail.com>
2024-10-10 22:07:32 +0300

    first conditon html content body

---
+++ PR_lab1/Main.cs
@@ -0,0 +1,13 @@
+﻿namespace PR_lab1;
+
+public class Lab1
+{
+    public static void Main()
+    {
+        WebScraper scraper = new();
+        
+        string url = "https://www.google.com";
+        string html = scraper.GetPageAsync(url).Result;
+        Console.WriteLine(html);
+    }
+}
---
+++ PR_lab1/WebScraper.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+class WebScraper
+{
+    public async Task<string> GetPageAsync(string url)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            HttpResponseMessage response = await client.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                string htmlContent = await response.Content.ReadAsStringAsync();
+                return htmlContent;
+            }
+            return null;
+        }
+    }
+}
